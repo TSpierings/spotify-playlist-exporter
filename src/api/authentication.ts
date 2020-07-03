2782ca92ba2e3b2411d2c05327a419297d7835be
Tchaka Spierings <tchaka.spierings@gmail.com>
2020-07-03 17:27:30 +0200

    Add authentication code

---
+++ src/api/authentication.ts
@@ -0,0 +1,17 @@
+/**
+ * All authentication constants necessary for Spotify's authentication API.
+ */
+export const AuthenticationDetails = {
+  uri: 'https://accounts.spotify.com/authorize',
+  clientId: '77d8475d33b74e52880600e79e98691d',
+  responseType: 'token',
+  redirectUri: 'http://localhost:3000/authenticate',
+  scopes: 'user-read-private'
+}
+
+/**
+ * A redirect to Spotify's authenticate URI with the necessary authentication details.
+ */
+export function authenticate() {
+  window.location.href = `${AuthenticationDetails.uri}?client_id=${AuthenticationDetails.clientId}&response_type=${AuthenticationDetails.responseType}&redirect_uri=${AuthenticationDetails.redirectUri}&scopes=${AuthenticationDetails.scopes}`;
+}
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { authenticate } from '../../api/authentication';
+import './home.scss';
 
 export class Home extends React.Component<{}, {}> {
   render() {
-    return <h1>Home</h1>;
+    return <div className="content">
+      <header>
+        <h1>Spotify Playlist Exporter</h1>
+      </header>
+      <section className="info">
+        <p>This application requires permission to search the spotify library for tracks/artists/genres. For that we require your authorization.</p>
+        <p>By clicking the button below, you will be redirected to Spotify, there you need to login and authorize the app for the mentioned permissions.</p>
+      </section>
+      <section className="authorization">
+        <button onClick={() => authenticate()}>Click here to authenticate Spotify</button>
+      </section>   
+    </div>;
   }
 }
