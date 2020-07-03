88ba987639941ff0e845b28bfa2010a6a4943c11
Tchaka Spierings <tchaka.spierings@gmail.com>
2020-07-03 17:48:36 +0200

    Fix and add scopes

--- src/api/authentication.ts
+++ src/api/authentication.ts
@@ -6,12 +6,12 @@ export const AuthenticationDetails = {
   clientId: '77d8475d33b74e52880600e79e98691d',
   responseType: 'token',
   redirectUri: 'http://localhost:3000/authenticate',
-  scopes: 'user-read-private'
+  scopes: 'playlist-read-private playlist-read-collaborative'
 }
 
 /**
  * A redirect to Spotify's authenticate URI with the necessary authentication details.
  */
 export function authenticate() {
-  window.location.href = `${AuthenticationDetails.uri}?client_id=${AuthenticationDetails.clientId}&response_type=${AuthenticationDetails.responseType}&redirect_uri=${AuthenticationDetails.redirectUri}&scopes=${AuthenticationDetails.scopes}`;
+  window.location.href = `${AuthenticationDetails.uri}?client_id=${AuthenticationDetails.clientId}&response_type=${AuthenticationDetails.responseType}&redirect_uri=${AuthenticationDetails.redirectUri}&scope=${AuthenticationDetails.scopes}`;
 }
